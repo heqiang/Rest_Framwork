a177dccfa39d24a78e0b22c114400df27f5bca85
hq <1422127065@qq.com>
2020-09-17 17:33:47 +0800

    Add 序列化器 字段超链接未解决(url反向解析)

--- classbaseview/urls.py
+++ classbaseview/urls.py
@@ -3,9 +3,9 @@ from classbaseview import views
 from django.conf.urls  import url
 
 urlpatterns = [
-        #
-        url(r'^(?P<versions>[v1|v2]+)/users/$',views.UserView.as_view(),name="User"),
-        url(r'^(?P<versions>[v1|v2]+)/userinfo/$',views.UserinfoView.as_view(),name="userinfo")
+        url(r'^(?P<versions>[v1|v2]+)/users/$', views.UserView.as_view(),name="User"),
+        url(r'^(?P<versions>[v1|v2]+)/userinfo/$', views.UserInfoView.as_view(),name="userinfo"),
+        url(r'^(?P<versions>[v1|v2]+)/group/(?P<pk>\d+)/$', views.GroupView.as_view(),name='ttt'),
 ]
 
 
